7b8eb26e218e9c53c70e6af427a0f2f84bcb5375
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-15 21:31:45 +0200

    feat: Create Squeezed property and Squeeze method

--- src/Codecool.WasteRecycling/PaperGarbage.cs
+++ src/Codecool.WasteRecycling/PaperGarbage.cs
@@ -2,5 +2,11 @@ namespace Codecool.WasteRecycling
 {
     public class PaperGarbage : Garbage
     {
+        public bool Squeezed { get; private set; }
+
+        public void Squeeze()
+        {
+            Squeezed = true;
+        }
     }
 }
