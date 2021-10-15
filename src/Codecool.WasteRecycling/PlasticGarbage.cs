d76b993b02ad8c2e3e4f8a9584a2ab90c243a22e
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-15 21:31:59 +0200

    feat: Create Cleaned property and Clean method

--- src/Codecool.WasteRecycling/PlasticGarbage.cs
+++ src/Codecool.WasteRecycling/PlasticGarbage.cs
@@ -2,5 +2,11 @@ namespace Codecool.WasteRecycling
 {
     public class PlasticGarbage : Garbage
     {
+        public bool Cleaned { get; private set; }
+
+        public void Clean()
+        {
+            Cleaned = true;
+        }
     }
 }
