0913ad16464ffc4f19fdbe6acef7c9e515c838b2
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-16 13:24:56 +0200

    feat: Add base ctor to PaperGarbage

--- src/Codecool.WasteRecycling/PaperGarbage.cs
+++ src/Codecool.WasteRecycling/PaperGarbage.cs
@@ -2,6 +2,10 @@ namespace Codecool.WasteRecycling
 {
     public class PaperGarbage : Garbage
     {
+        public PaperGarbage(string name) :
+            base(name)
+        {
+        }
         public bool Squeezed { get; private set; }
 
         public void Squeeze()
