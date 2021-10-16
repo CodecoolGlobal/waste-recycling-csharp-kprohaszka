b1470afd2494b0f38ccfb522917955d1d3fa46c0
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-16 13:31:37 +0200

    feat: Create eight garbage instaces

--- src/Codecool.WasteRecycling/Program.cs
+++ src/Codecool.WasteRecycling/Program.cs
@@ -8,6 +8,14 @@ namespace Codecool.WasteRecycling
         public static void Main(string[] args)
         {
             Dustbin dustbin = new Dustbin("blue");
+            var regularGarbage1 = new Garbage("Chewed Apple");
+            var regularGarbage2 = new Garbage("Something weird and moldy");
+            var paperGarbage1 = new PaperGarbage("Unfinished Essay");
+            var paperGarbage2 = new PaperGarbage("Unpayed Bills");
+            var paperGarbage3 = new PaperGarbage("House Mortgage");
+            var plasticGarbage1 = new PlasticGarbage("Mineral Water Bottle");
+            var plasticGarbage2 = new PlasticGarbage("McDonalds Fork");
+            var plasticGarbage3 = new PlasticGarbage("Wrapper");
         }
     }
 }
