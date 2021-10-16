79563ca4fb4c932e5c32513fdbb28acd8e4537a1
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-16 13:21:11 +0200

    feat: Add base ctor to PlasticGarbage

--- src/Codecool.WasteRecycling/PlasticGarbage.cs
+++ src/Codecool.WasteRecycling/PlasticGarbage.cs
@@ -2,6 +2,11 @@ namespace Codecool.WasteRecycling
 {
     public class PlasticGarbage : Garbage
     {
+        public PlasticGarbage(string name) :
+            base(name)
+        {
+        }
+
         public bool Cleaned { get; private set; }
 
         public void Clean()
