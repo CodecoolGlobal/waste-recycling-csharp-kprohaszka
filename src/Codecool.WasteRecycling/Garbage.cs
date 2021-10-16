a6f085c3a4db4515ea7819274ff7af9715638cad
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-16 13:15:09 +0200

    feat: Create ctor for Garbage with name as a must

--- src/Codecool.WasteRecycling/Garbage.cs
+++ src/Codecool.WasteRecycling/Garbage.cs
@@ -2,6 +2,11 @@ namespace Codecool.WasteRecycling
 {
     public class Garbage
     {
+        public Garbage(string name)
+        {
+            Name = name;
+        }
+
         public string Name {get; private set;}
     }
 }
